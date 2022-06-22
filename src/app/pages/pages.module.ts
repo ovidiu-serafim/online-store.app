eac99902b452e440a938208753fd2f145e2e27e2
ovidiu <ovidiu.popescu@gsdgroup.net>
2022-06-22 17:43:16 +0300

    create cart component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import {PagesRoutingModule} from "./pages.routing-module";
+import { CartComponent } from './cart/cart.component';
+import {UiModule} from "../ui/ui.module";
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    CartComponent
   ],
   imports: [
     CommonModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    UiModule
   ]
 })
 export class PagesModule { }
--- src/app/pages/pages.routing-module.ts
+++ src/app/pages/pages.routing-module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {HomeComponent} from './home/home.component';
 import {RouterModule, Routes} from "@angular/router";
+import {CartComponent} from "./cart/cart.component";
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     component: HomeComponent,
     path: 'home'
+  },
+  {
+    component: CartComponent,
+    path: 'cart'
   }
 ];
 
