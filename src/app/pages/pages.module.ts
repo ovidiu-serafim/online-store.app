2d6cfc2a9711728392940e4594f8963ab128bd9e
Alex Hampu <alex.hampu@deploy.com.ro>
2022-06-23 11:40:04 +0300

    fix style

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HomeComponent} from './home/home.component';
 import {PagesRoutingModule} from "./pages.routing-module";
 import {ProductCardComponent} from "../ui/product-card/product-card.component";
 import {UiModule} from "../ui/ui.module";
 import {MatButtonModule} from "@angular/material/button";
-import { CartComponent } from './cart/cart.component';
+import {CartComponent} from './cart/cart.component';
 
 
 @NgModule({
@@ -19,11 +19,12 @@ import { CartComponent } from './cart/cart.component';
     ProductCardComponent,
     CartComponent
   ],
-    imports: [
-        CommonModule,
-        PagesRoutingModule,
-        UiModule,
-        MatButtonModule
-    ]
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    UiModule,
+    MatButtonModule
+  ]
 })
-export class PagesModule { }
+export class PagesModule {
+}
