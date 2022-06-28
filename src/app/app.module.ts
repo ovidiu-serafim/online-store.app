fc57b2c0d94c6e6cc4b2324860da39df5a163716
ovidiu <ovidiu.popescu@gsdgroup.net>
2022-06-28 14:57:07 +0300

    remove empty line

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -66,5 +65,4 @@ import { SizeSelectorComponent } from './ui/size-selector/size-selector.componen
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
